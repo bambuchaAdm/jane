7d608c69c30e094adb9ff4427abada4e6daba1aa
Łukasz Dubiel <bambucha14@gmail.com>
2014-06-15 18:18:39 +0200

    Add stashing information before connection is maked.

--- src/main/scala/org/bambucha.watson/IRCConnection.scala
+++ src/main/scala/org/bambucha.watson/IRCConnection.scala
@@ -1,23 +1,26 @@
 package org.bambucha.watson
 
-import akka.actor.{ActorRef, ActorLogging, Props, Actor}
+import akka.actor._
 import akka.io.{Tcp, IO}
 import akka.io.Tcp._
 import java.net.InetSocketAddress
 import org.bambucha.watson.messages._
+import akka.util.ByteString
+import java.nio.charset.Charset
+import org.bambucha.watson.messages.NickCommand
 import akka.io.Tcp.Connected
 import akka.io.Tcp.Register
 import akka.io.Tcp.Connect
+import org.bambucha.watson.messages.UserCommand
+import org.bambucha.watson.messages.PasswordCommand
 import akka.io.Tcp.CommandFailed
-import akka.util.ByteString
-import java.nio.charset.Charset
 
 object IRCConnectionProtocol {
   object Start
   object AuthenticateToServer
 }
 
-class IRCConnection(dispatcher: ActorRef, nick: String, username: String, isWallops: Boolean, isInvisible: Boolean, realName: String) extends Actor with ActorLogging {
+class IRCConnection(dispatcher: ActorRef, nick: String, username: String, isWallops: Boolean, isInvisible: Boolean, realName: String) extends Actor with ActorLogging with Stash {
 
   import IRCConnectionProtocol._
 
@@ -45,28 +48,31 @@ class IRCConnection(dispatcher: ActorRef, nick: String, username: String, isWall
     }
   }
 
-  override def receive: Actor.Receive = {
-    case Start =>
-      log.debug("Attempt to connecting")
-      manager.tell(Connect(new InetSocketAddress("irc.freenode.net",6667)), lexerActor)
-    case Connected(remote, local) =>
-      connection = sender()
-      connection ! Register(lexerActor)
-      log.debug("Connected to {} using port {}", remote.getHostName, local.getPort)
+  def sendMessages: Actor.Receive = {
     case CommandFailed(command) =>
       log.debug(s"Command failed -> ${command.failureMessage}")
     case command: IRCCommand =>
       val message = command.toMessage
+      log.debug(s"Message to send {}", message)
       val rawString = Seq(
         message.command,
         formatParameters(message)
       ).fold("")(_ + _)
       val payload = ByteString(rawString.getBytes(charset))
+      log.debug(s"Try to send '$payload'")
       connection ! Write(payload)
   }
 
-
-
-
-
+  override def receive: Actor.Receive = {
+    case Start =>
+      log.debug("Attempt to connecting")
+      manager ! Connect(new InetSocketAddress("irc.freenode.net",6667))
+    case Connected(remote, local) =>
+      context.become(sendMessages)
+      unstashAll()
+      connection = sender()
+      connection ! Register(lexerActor)
+      log.debug("Connected to {} using port {}", remote.getHostName, local.getPort)
+    case _ => stash()
+  }
 }
